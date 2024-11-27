80a8121dc0f2adc31136378edb52e68a39c2b752
NoeGrü <grunbaum.noel@gmail.com>
2024-11-27 16:05:28 +0100

    moved scripts to scripts folder, imported 3 sec count down sound and made it play before toggling, further work on level

--- scripts/platform_toggle.gd
+++ scripts/platform_toggle.gd
@@ -3,27 +3,22 @@ extends Node3D
 @export var isGreen : bool
 @export var colorGreen : Material
 @export var colorRed : Material
-@onready var platform_2: MeshInstance3D = $platform2
+@onready var mesh: MeshInstance3D = $platform2
 
 @onready var collision: CollisionShape3D = $StaticBody3D/CollisionShape3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if isGreen:
-		platform_2.set_surface_override_material(0,colorGreen)
+		mesh.set_surface_override_material(0,colorGreen)
 		#Green platforms start turned on
 		visible = false
 		collision.disabled = true
 	else:
-		platform_2.set_surface_override_material(0,colorRed)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		mesh.set_surface_override_material(0,colorRed)
 
 
 func toggleState(isGreenOn : bool) -> void:
-	print(isGreenOn)
 	if((isGreen and isGreenOn) or (not isGreen and not isGreenOn)):
 		visible = true
 		collision.set_deferred("disabled", false)
--- scripts/platform_toggle_controller.gd
+++ scripts/platform_toggle_controller.gd
@@ -3,6 +3,8 @@ extends Node
 @export var isGreenOn : bool
 @export var timerTrigger : bool
 @onready var timer : Timer = $Timer
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+var timerSoundPlayed : bool
 
 
 func receive_input(state_on) -> void:
@@ -10,9 +12,14 @@ func receive_input(state_on) -> void:
 	isGreenOn = state_on
 	get_tree().call_group("TogglePlatform", "toggleState", isGreenOn)
 
-
+func _process(_delta: float) -> void:
+	if((timerTrigger) and (timer.time_left <= 3) and (not timerSoundPlayed)):
+		audio_stream_player.play()
+		timerSoundPlayed = true
+	
 func _on_timer_timeout() -> void:
 	if(timerTrigger):
 		#Toggles between green and red
+		timerSoundPlayed = false
 		isGreenOn = not isGreenOn
 		get_tree().call_group("TogglePlatform", "toggleState", isGreenOn)
